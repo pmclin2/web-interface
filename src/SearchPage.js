fbd520455fe9a5dd3e89a8d376aeb4e8682bb96c
Evan Allender <evan.allender@gmail.com>
2023-12-11 14:32:47 -0500

    searchPage sanitization

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,38 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
-import AppBar from '@mui/material/AppBar';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Toolbar from '@mui/material/Toolbar';
-import Container from '@mui/material/Container';
-import { List, ListItemButton, ListItemText } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import AppBar from "@mui/material/AppBar";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import Container from "@mui/material/Container";
+import { List, ListItemButton, ListItemText } from "@mui/material";
+import { escape } from "lodash";
 
 const SearchPage = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQuery, setSearchQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const defaultTheme = createTheme();
   const [data, setData] = useState(null);
 
   useEffect(() => {
-    const AWS = require('aws-sdk');
+    const AWS = require("aws-sdk");
     AWS.config.update({
-      accessKeyId: sessionStorage.getItem('accessKey'),
-      secretAccessKey: sessionStorage.getItem('secretAccessKey'),
-      region: 'us-east-1'
+      accessKeyId: sessionStorage.getItem("accessKey"),
+      secretAccessKey: sessionStorage.getItem("secretAccessKey"),
+      region: "us-east-1",
     });
 
     const dynamodb = new AWS.DynamoDB();
 
     const params = {
-      TableName: 'registry'
+      TableName: "registry",
     };
 
     dynamodb.scan(params, (err, responseData) => {
       if (err) {
-        console.error('Error fetching data:', err);
+        console.error("Error fetching data:", err);
       } else {
         setData(responseData);
       }
@@ -42,11 +43,15 @@ const SearchPage = () => {
   const handleSearch = () => {
     // Filter mock data based on search query
 
-    // Input Sanitization 'searchQuery'
+    // Input sanitization
+    let sanitized_input = searchQuery;
+    sanitized_input = escape(sanitized_input_input);
+    sanitized_input.name = sanitized_input.name.replace(/[^a-zA-Z0-9.]/g, "_");
+    sanitized_input.name = sanitized_input.name.replace(/_+/g, "_");
+    // End input sanitization
 
-    const filteredResults = data.Items.filter(
-      (packageData) =>
-        packageData.name.S.toLowerCase().includes(searchQuery.toLowerCase())
+    const filteredResults = data.Items.filter((packageData) =>
+      packageData.name.S.toLowerCase().includes(user_input.toLowerCase())
     );
 
     setSearchResults(filteredResults);
@@ -63,21 +68,25 @@ const SearchPage = () => {
       </AppBar>
       <Box
         sx={{
-          bgcolor: 'background.paper',
+          bgcolor: "background.paper",
           pt: 4,
           pb: 6,
           px: 4,
         }}
       >
         {/* Add "Back to Homepage" button */}
-        <Link to="/" style={{ textDecoration: 'none' }}>
-          <Button variant="outlined">
-            Back
-          </Button>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <Button variant="outlined">Back</Button>
         </Link>
         <Container maxWidth="sm">
-          <Typography variant="h5" align="center" color="text.secondary" paragraph>
-            Search for any packages that have already been uploaded to the registry.
+          <Typography
+            variant="h5"
+            align="center"
+            color="text.secondary"
+            paragraph
+          >
+            Search for any packages that have already been uploaded to the
+            registry.
           </Typography>
         </Container>
         <Container maxWidth="lg">
@@ -85,17 +94,24 @@ const SearchPage = () => {
             placeholder="Enter package name"
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
-            sx={{ mt: 2, 
-                  mb: 2, 
-                  mr: 2,
-                  width: '100%'
-            }}
+            sx={{ mt: 2, mb: 2, mr: 2, width: "100%" }}
           />
-          <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-            <Button variant="contained" onClick={handleSearch} disabled={searchQuery.trim().length === 0} sx={{ mt: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-start",
+            }}
+          >
+            <Button
+              variant="contained"
+              onClick={handleSearch}
+              disabled={searchQuery.trim().length === 0}
+              sx={{ mt: 2 }}
+            >
               Search
             </Button>
-            <List sx={{ width: '100%' }}>
+            <List sx={{ width: "100%" }}>
               {searchResults.map((result) => (
                 <ListItemButton
                   key={result.name.S}
