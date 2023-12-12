90545fd2636f5e62341e624d1f919a7a4625cb19
ayushuk <ayushshukla30339@gmail.com>
2023-12-12 12:13:09 -0500

    add rest api calls

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,64 +1,72 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import List from '@mui/material/List';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-//import AWS_CONFIG from './awsConfig';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import axios from "axios";
+
+const BASE_URL =
+  "https://4n1pa9gczk.execute-api.us-east-1.amazonaws.com/Deployment";
 
 const HomePage = () => {
   const defaultTheme = createTheme();
-  const [data, setData] = useState(null);
-  const [accessKey, setAccessKey] = useState('');
-  const [secretAccessKey, setSecretAccessKey] = useState('');
+  const [data, setData] = useState({
+    Items: [
+      {
+        id: { S: "fecha" },
+        name: { S: "fecha" },
+      },
+      {
+        id: { S: "easy-math-module" },
+        name: { S: "Easy math" },
+      },
+    ],
+  });
+  const [accessKey, setAccessKey] = useState("");
+  const [secretAccessKey, setSecretAccessKey] = useState("");
 
   const handleLogout = () => {
-    setAccessKey('');
-    setSecretAccessKey('');
+    setAccessKey("");
+    setSecretAccessKey("");
 
-    sessionStorage.setItem('accessKey', accessKey);
-    sessionStorage.setItem('secretAccessKey', secretAccessKey);
+    sessionStorage.setItem("accessKey", accessKey);
+    sessionStorage.setItem("secretAccessKey", secretAccessKey);
   };
 
   useEffect(() => {
-    const AWS = require('aws-sdk');
-    AWS.config.update({
-      accessKeyId: sessionStorage.getItem('accessKey'),
-      secretAccessKey: sessionStorage.getItem('secretAccessKey'),
-      region: 'us-east-1'
-    });
-
-    const dynamodb = new AWS.DynamoDB();
-
-    const params = {
-      TableName: 'registry'
-    };
-
-    dynamodb.scan(params, (err, responseData) => {
-      if (err) {
-        console.error('Error fetching data:', err);
-      } else {
-        setData(responseData);
-      }
-    });
+    // axios
+    //   .get(`${BASE_URL}/packages`)
+    //   .then((response) => {
+    //     console.log(response);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    // temp until /packages is done
   }, []);
 
   return (
     <div>
       <ThemeProvider theme={defaultTheme}>
         <AppBar position="relative">
-          <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Toolbar sx={{ justifyContent: "space-between" }}>
             <Typography variant="h6" color="inherit" noWrap>
               ACME Corporation Package Registry
             </Typography>
-            <Button color="inherit" onClick={handleLogout} component={Link} to="/">
+            <Button
+              color="inherit"
+              onClick={handleLogout}
+              component={Link}
+              to="/"
+            >
               Logout
             </Button>
           </Toolbar>
@@ -66,7 +74,7 @@ const HomePage = () => {
         <main>
           <Box
             sx={{
-              bgcolor: 'background.paper',
+              bgcolor: "background.paper",
               pt: 8,
               pb: 6,
             }}
@@ -105,16 +113,20 @@ const HomePage = () => {
               Package Directory
             </Typography>
             <List>
-              {data && data.Items && data.Items.length > 0 && data.Items.map((item) => (
-                <ListItemButton
-                  key={item.id.S} // Assuming 'id' is a string attribute (use .S for string type)
-                  component={Link}
-                  to={`/package/${item.name.S}`} // Assuming 'name' is a string attribute
-                  divider
-                >
-                  <ListItemText primary={`${item.name.S}`} /> {/* Display name */}
-                </ListItemButton>
-              ))}
+              {data &&
+                data.Items &&
+                data.Items.length > 0 &&
+                data.Items.map((item) => (
+                  <ListItemButton
+                    key={item.id.S} // Assuming 'id' is a string attribute (use .S for string type)
+                    component={Link}
+                    to={`/package/${item.id.S}`} // Assuming 'name' is a string attribute
+                    divider
+                  >
+                    <ListItemText primary={`${item.name.S}`} />{" "}
+                    {/* Display name */}
+                  </ListItemButton>
+                ))}
             </List>
           </Container>
         </main>
